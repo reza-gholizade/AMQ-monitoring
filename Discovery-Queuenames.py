f77689554b93e1cf50777d3f6f9b61b87b7baea5
rezafabregas <gholizade.net@gmail.com>
2018-09-20 03:55:01 +0430

    Update Discovery-Queuenames.py

--- Discovery-Queuenames.py
+++ Discovery-Queuenames.py
@@ -9,7 +9,7 @@ def jsonparser():
    result = json.loads(r.content)
    for item in result['value'].keys():
        qnames = item.split(",")[1].split("=")[1]
-       data.append('{#01NAME}':quames})
+       data.append('{#QNAME}':quames})
        return json.dumps({"data":data})
 
 urll = http://{}:{}/api/jolokia/read/org.apache.activemq:brokerName=localhost,destinationName=*,destinationType=Queue,type=Broker.format(sys.argv[1], sys.argv[2]) 
