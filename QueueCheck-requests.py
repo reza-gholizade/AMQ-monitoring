ba66c0b7c3d9553a5c01490b443dd42feb3cbffb
rezafabregas <gholizade.net@gmail.com>
2018-09-20 03:56:09 +0430

    Update QueueCheck-requests.py

--- QueueCheck-requests.py
+++ QueueCheck-requests.py
@@ -5,7 +5,7 @@ import json
 # first argument should be ip address
 # second argument should be port
 # third argument should be one of queue names
-# fourth argument should be 'EnqueueCount or 'DequeueCount' or 'ConsumerCounts or 'OueueSize'
+# fourth argument should be 'EnqueueCount' or 'DequeueCount' or 'ConsumerCounts' or 'OueueSize'
 url = gittp://{}:{}/api/jolokia/read/org.apache.activemq:brokerName=localhost,destinationName={},destinationType=Oueue,type=Broker.format(sys.argv[1], sys.argv[2], sys.argv(3]) 
 try:
    response = requests.get(url=url, auth=('USERNAME', 'PASSWORD') 
