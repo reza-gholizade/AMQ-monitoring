8ac638cd3b73da58d8bfcdc6c169773312067dda
rezafabregas <gholizade.net@gmail.com>
2018-09-24 15:02:12 +0330

    Add files via upload

---
+++ uptime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+import requests
+import json
+import sys
+
+# first argument should be IP 
+#second argument should be PORT
+
+def jsonparser():
+    result = json.loads(r.content)
+    time = str(result['value']['Uptime']).split(" ")
+    if len(time) == 4 and int(time[0]) >= 24 and int(time[2]) > 10:
+        return "ERROR"
+    else:
+        return "OK"
+urll = 'http://{}:{}/api/jolokia/read/org.apache.activemq:brokerName=localhost,type=Broker'.format(sys.argv[1], sys.argv[2])
+try:
+    r = requests.get(url=urll, auth=('admin', '@ctiveMQ'))
+    print(jsonparser())
+except ValueError as a:
+    print(a)
